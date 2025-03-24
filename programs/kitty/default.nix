d92d7b10f3abb3f4cb6baf2f15d907925ee8b592
Salar Rahmanian <code@softinio.com>
2025-03-23 17:23:59 -0700

    Improvements to kitty conf to support hyperlinks

---
+++ programs/kitty/default.nix
@@ -0,0 +1,32 @@
+{ pkgs, ... }:
+
+let
+  actionsConfig = builtins.readFile ./open-actions.conf;
+in
+{
+  programs.kitty = {
+    enable = true;
+    keybindings = {
+      "kitty_mod+enter" = "new_window_with_cwd";
+      "f1" = "create_marker";
+      "f2" = "remove_marker";
+      "ctrl+p" = "scroll_to_mark prev";
+      "ctrl+n" = "scroll_to_mark next";
+    };
+    themeFile = "Tango_Dark";
+    font = {
+      name = "SF Mono";
+      size = 13;
+    };
+    settings = {
+      copy_on_select = true;
+      enabled_layouts = "*";
+      kitty_mod = "cmd+option";
+      macos_quit_when_last_window_closed = true;
+      scrollback_lines = 100000;
+    };
+    shellIntegration.enableFishIntegration = true;
+  };
+
+  home.file.".config/kitty/open-actions.conf".text = actionsConfig;
+}
